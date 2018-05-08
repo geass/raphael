112d396ee1214ea9911c211364d800f1b0363034
muhammet arslan <muhammet.arslan@sony.com>
2018-05-08 17:35:54 +0300

    oauth2

--- raphael/apps/restaurants/views.py
+++ raphael/apps/restaurants/views.py
@@ -1,5 +1,6 @@
 from .models import Restaurants
-from rest_framework import viewsets
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
+from rest_framework import permissions, routers, serializers, viewsets
 from .serializers import RestaurantsSerializer
 
 
@@ -7,5 +8,6 @@ class RestaurantsViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Restaurants.objects.all()
     serializer_class = RestaurantsSerializer
--- raphael/urls.py
+++ raphael/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 from rest_framework import routers
 from raphael.apps.restaurants.views import RestaurantsViewSet
 
@@ -6,6 +7,8 @@ router = routers.DefaultRouter()
 router.register(r'restaurants', RestaurantsViewSet)
 
 urlpatterns = [
+	url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
